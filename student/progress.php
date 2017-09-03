288c568cbf211a9a3584708bc2b72a0f3041e2ff
Emoblazz <emoblazz@gmail.com>
2017-09-03 23:39:10 +0800

    progresss fixes

--- student/progress.php
+++ student/progress.php
@@ -1,6 +1,5 @@
 <?php 
 include('session.php');
-error_reporting(0);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -33,63 +32,105 @@ error_reporting(0);
 						<div class="row">
 							<div class="col-md-12">
 								<!-- BEGIN PORTLET -->
-								<div class="portlet box blue">
+								<div class="portlet box grey">
 						<div class="portlet-title">
 							<div class="caption">
 								<i class="fa fa-edit"></i> Progress
 							</div>
 						</div>
 						<div class="portlet-body">
+							<div class="table-toolbar">
+								<div class="row">
+									<div class="col-md-6">
+										<div class="btn-group">
+											<a href="export.php?gid=<?php echo $_REQUEST['gid'];?>" id="sample_editable_1_new" class="btn green">
+											Export <i class="fa fa-plus"></i>
+											</a>
+										</div>
+									</div>
+									
+									
+								</div>
+							</div>
 							<table class="table table-striped table-hover table-bordered" id="sample_editable_1">
-							
-							
+							<thead>
+							<tr>
+								<th>
+									 Name
+								</th>
 <?php 
 	include('../includes/dbcon.php');
 	if (isset($_REQUEST['nsid']))
-	{	
-		$nsid=$_REQUEST['nsid'];
-		mysqli_query($con,"UPDATE notif_stat SET read_status='1' where notif_stat_id='$nsid'")
-	 or die(mysqli_error()); 
+		{	
+			$nsid=$_REQUEST['nsid'];
+			mysqli_query($con,"UPDATE notif_stat SET read_status='1' where notif_stat_id='$nsid'")
+		 or die(mysqli_error()); 
 
-	}
-        $gid=$_REQUEST['gid'];
-        $id=$_SESSION['id'];
-        
-            $at=mysqli_query($con,"SELECT * FROM `grade` join post where grade.post_id=post.post_id and grade.group_id='$gid' and grade.member_id='$id'")or die(mysqli_error($con));
-							    while($row=mysqli_fetch_array($at))
-							    {
-							    $gid=$row['grade_id'];	
-										      
-							      echo "<tr><th>$row[post_title]</th>";
-							      echo "<th>$row[score]/$row[total]</th>";
-							       if ($row['type']=="assignment")
-							      {
-							      
-							      	echo "<th></th>";
-		
-							      }
-	
+		}
+
+	        $gid=$_REQUEST['gid'];
+	            $at=mysqli_query($con,"select * from post natural join group_post where group_id='$gid' and post_type='assignment'")or die(mysqli_error($con));
+								    while($row=mysqli_fetch_array($at))
+								    {		      
+								      echo "<th>$row[post_title]</th>";
+
+								     } 
+			$quiz=mysqli_query($con,"select * from quiz natural join group_quiz where group_id='$gid'")or die(mysqli_error($con));
+								    while($rowq=mysqli_fetch_array($quiz))
+								    {		     
+								      $qid=$rowq['quiz_id']; 
+								      echo "<th>$rowq[quiz_title]</th>";
+								      //echo "<a href='stat1.php?qid=$qid&gid=$gid'>
+								      	//	<i class='icon-bar-chart font-blue'></i> </a></th>";
+
+								     } 
 ?>
-							
 							</tr>
-													
-<?php }?>
+							</thead>
+							<tbody>
+<?php
+	$id=$_SESSION['id'];
+	$query1=mysqli_query($con,"select * from enrol natural join member where group_id='$gid' and status='approved' and member_id='$id' order by member_last")or die(mysqli_error());
+		
+		$countassign=mysqli_num_rows($query1);
+		if ($countassign<1) echo "
+			<div class='alert alert-danger'>
+				You have members yet!
+			</div>";
+			while($row2=mysqli_fetch_array($query1))
+			{
+				$member_id=$row2['member_id'];
+			
+?>								
+							<tr>
+								<td>
+									 <?php echo $row2['member_last'];?>, 
+									 <?php echo $row2['member_first'];?>
+								</td>
 <?php 
+            $loop=mysqli_query($con,"select * from post natural join group_post where group_id='$gid' and post_type='assignment'")or die(mysqli_error());
+					
+					while($row=mysqli_fetch_array($loop))
+					{	
+						$gpid=$row['group_post_id'];
 
-            $quiz=mysqli_query($con,"SELECT * FROM `grade` join quiz where grade.quiz_id=quiz.quiz_id and grade.group_id='$gid' and grade.member_id='$id'")or die(mysqli_error($con));
-							    while($q=mysqli_fetch_array($quiz))
-							    {
-										
-								$qid=$q['quiz_id'];		      
-							      echo "<tr><th>$q[quiz_title]</th>";
-							      echo "<th>$q[score]/$q[total]</th>";
+						$grade=mysqli_query($con,"select * from submission natural join grade where group_post_id='$gpid' and member_id='$member_id'")or die(mysqli_error());
 
-							      
-							      	echo "<th><a href='stat.php?qid=$qid'>
-							      	<i class='icon-bar-chart font-blue'></i> </a></th>";
-							      
+						$rowg=mysqli_fetch_array($grade);
+						echo "<th>$rowg[score]/$rowg[total]</th>";
+					}
+
+			$quiz=mysqli_query($con,"select * from quiz natural join group_quiz where group_id='$gid'")or die(mysqli_error($con));
+					while($rowq=mysqli_fetch_array($quiz))
+					   {		
+					   		$qid=$rowq['quiz_id'];      
+							$quizloop=mysqli_query($con,"select * from quiz_result natural join grade where group_id='$gid' and member_id='$member_id' and quiz_id='$qid'")or die(mysqli_error($con));
+						$rowql=mysqli_fetch_array($quizloop);
+						echo "<th>$rowql[score]/$rowql[total]</th>";	     
+					    } 		
 ?>
-							
+
+								
 							</tr>
 													
 <?php }?>							
